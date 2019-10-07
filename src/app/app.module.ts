a633dde036e588c01d5f5f1348a4b820be311f83
Ana <anastasy.d@gmail.com>
2019-10-07 21:35:00 +0200

    Generate gallery and tile components, cleanup app placeholder content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TileComponent } from './tile/tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    TileComponent
   ],
   imports: [
     BrowserModule
